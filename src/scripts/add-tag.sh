4b0e064e37a36d4525e2675badb4c40f58063da6
Olekasndr Kosenko <sashao@dbmaestro.com>
2021-09-09 13:29:56 +0300

    [semver:ignore] add tag fix

--- src/scripts/add-tag.sh
+++ src/scripts/add-tag.sh
@@ -1,9 +1,9 @@
 AddTag(){
-    CMDLINE="java -jar \"$DBM_TOOL_PATH/DBmaestroAgent.jar\" -Validate -ProjectName \"$DBM_PROJECT_NAME\" -PackageName \"$DBM_PACKAGE_NAME\""
+    CMDLINE='java -jar "'$DBM_TOOL_PATH'/DBmaestroAgent.jar" -Validate -ProjectName "'$DBM_PROJECT_NAME'" -PackageName "'$DBM_PACKAGE_NAME'"'
     if [[ ! -z ${OBJ_TYPE_NAME} && ! -z ${OBJ_NAME} ]]; then
-        CMDLINE=$CMDLINE"-ObjectTypeName \"$OBJ_TYPE_NAME\" -ObjectName \"$OBJ_NAME\""
+        CMDLINE=$CMDLINE'-ObjectTypeName "'$OBJ_TYPE_NAME'" -ObjectName "'$OBJ_NAME'"'
     fi
-    CMDLINE=$CMDLINE" -Server \"$DBM_SERVER_ADDRESS\" -AuthType \"$DBM_AUTH_TYPE\" -UserName \"$DBM_USERNAME\" -Password \"$DBM_PASSWORD\""
+    CMDLINE=$CMDLINE' -Server "'$DBM_SERVER_ADDRESS'" -AuthType "'$DBM_AUTH_TYPE'" -UserName "'$DBM_USERNAME'" -Password "'$DBM_PASSWORD'"'
     $CMDLINE
 }
 
