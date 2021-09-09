68edd4ef74a1d1d38121ca482574e46ac5a23364
Olekasndr Kosenko <sashao@dbmaestro.com>
2021-09-09 13:44:20 +0300

    [semver:skip] add tag fix

--- src/scripts/add-tag.sh
+++ src/scripts/add-tag.sh
@@ -1,10 +1,12 @@
+#!/bin/bash
+
 AddTag(){
     CMDLINE='java -jar "'$DBM_TOOL_PATH'/DBmaestroAgent.jar" -Validate -ProjectName "'$DBM_PROJECT_NAME'" -PackageName "'$DBM_PACKAGE_NAME'"'
-    if [[ ! -z ${OBJ_TYPE_NAME} && ! -z ${OBJ_NAME} ]]; then
+    if [[  -n ${OBJ_TYPE_NAME} &&  -n ${OBJ_NAME} ]]; then
         CMDLINE=$CMDLINE'-ObjectTypeName "'$OBJ_TYPE_NAME'" -ObjectName "'$OBJ_NAME'"'
     fi
     CMDLINE=$CMDLINE' -Server "'$DBM_SERVER_ADDRESS'" -AuthType "'$DBM_AUTH_TYPE'" -UserName "'$DBM_USERNAME'" -Password "'$DBM_PASSWORD'"'
-    $CMDLINE
+    "$CMDLINE"
 }
 
 # Will not run if sourced for bats-core tests.
