9e827bcf2d42370515eff71594c4c77ae466393f
Olekasndr Kosenko <sashao@dbmaestro.com>
2021-09-12 16:18:37 +0300

    [semver:slip] draft

--- src/scripts/add-tag.sh
+++ src/scripts/add-tag.sh
@@ -1,11 +1,27 @@
 #!/bin/bash
 
 AddTag(){
+    if [ -z "$DBM_PASSWORD" ]
+    then
+      echo "Using global password variable"
+      DBM_PWD=$DBMAESTRO_PASSWORD
+    else
+      echo "Using local password variable"
+      DBM_PWD=$DBM_PASSWORD
+    fi
+    if [ -z "$DBM_PASSWORD" ]
+        then
+          echo "Using global password variable"
+          DBM_PWD=$DBMAESTRO_PASSWORD
+        else
+          echo "Using local password variable"
+          DBM_PWD=$DBM_PASSWORD
+        fi
     CMDLINE='java -jar "'$DBM_TOOL_PATH'/DBmaestroAgent.jar" -Validate -ProjectName "'$DBM_PROJECT_NAME'" -PackageName "'$DBM_PACKAGE_NAME'" -TagTypeName "'$DBM_TAG_TYPE_NAME'" -TagName "'$DBM_TAG_NAME'"'
     if [[  -n ${OBJ_TYPE_NAME} &&  -n ${OBJ_NAME} ]]; then
         CMDLINE=$CMDLINE'-ObjectTypeName "'$OBJ_TYPE_NAME'" -ObjectName "'$OBJ_NAME'"'
     fi
-    CMDLINE=$CMDLINE' -Server "'$DBM_SERVER_ADDRESS'" -AuthType "'$DBM_AUTH_TYPE'" -UserName "'$DBM_USERNAME'" -Password "'$DBM_PASSWORD'"'
+    CMDLINE=$CMDLINE' -Server "'$DBM_SERVER_ADDRESS'" -AuthType "'$DBM_AUTH_TYPE'" -UserName "'$DBM_USERNAME'" -Password "'$DBM_PWD'"'
     "$CMDLINE"
 }
 
