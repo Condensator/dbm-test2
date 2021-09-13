19e1d0cd2927ea9c57770c6a6168e7bf34a489a2
Olekasndr Kosenko <sashao@dbmaestro.com>
2021-09-13 16:49:12 +0300

    tool path

--- src/scripts/add-tag.sh
+++ src/scripts/add-tag.sh
@@ -25,7 +25,8 @@ java -version
     fi
     CMDLINE=$CMDLINE' -Server "'$DBM_SERVER_ADDRESS'" -AuthType "'$DBM_AUTH_TYPE'" -UserName "'$DBM_USERNAME'" -Password "'$DBM_PWD'"'
     echo "$CMDLINE"
-    java -jar  /"$DBM_TOOL_PATH"/DBmaestroAgent.jar/"$CMDLINE"
+    DBM_TOOL_PATH=$DBM_TOOL_PATH"/DBmaestroAgent.jar"
+    java -jar  "$DBM_TOOL_PATH""$CMDLINE"
 }
 
 # Will not run if sourced for bats-core tests.
