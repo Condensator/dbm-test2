be31280931809f5ee6ded85abcfb37b8f116ae2b
Olekasndr Kosenko <sashao@dbmaestro.com>
2021-09-14 15:35:36 +0300

    build

--- src/scripts/build.sh
+++ src/scripts/build.sh
@@ -9,18 +9,18 @@ Build(){
       echo "Using local password variable"
       DBM_PWD=$DBM_PASSWORD
     fi
-    CMDLINE='java -jar "'$DBM_TOOL_PATH'/DBmaestroAgent.jar" -Build -ProjectName "'$DBM_PROJECT_NAME'" -EnvName "'$DBM_ENVIRONMENT_NAME'"'
+    CMDLINE='java -jar "'$DBM_TOOL_PATH'/DBmaestroAgent.jar" -Build -ProjectName "'$DBM_PROJECT_NAME'" -EnvName "'$DBM_ENVIRONMENT_NAME'" '
     if [[  -n ${DBM_VERSION_TYPE} ]]; then
         CMDLINE=$CMDLINE'-VersionType "'$DBM_VERSION_TYPE' "'
     fi
     if [[  -n ${DBM_LABEL_NAME} ]]; then
-        CMDLINE=$CMDLINE'-LabelName "'$DBM_LABEL_NAME' "'
+        CMDLINE=$CMDLINE'-LabelName "'$DBM_LABEL_NAME'" '
     fi
     if [[  -n ${DBM_BASELINE_LABEL_NAME} ]]; then
-        CMDLINE=$CMDLINE'-BaselineLabelName "'$DBM_BASELINE_LABEL_NAME' "'
+        CMDLINE=$CMDLINE'-BaselineLabelName "'$DBM_BASELINE_LABEL_NAME'" '
     fi
     if [[  -n ${DBM_PACKAGE_NAME} ]]; then
-        CMDLINE=$CMDLINE'-CreatePackage -PackageName "'$DBM_PACKAGE_NAME' "'
+        CMDLINE=$CMDLINE'-CreatePackage -PackageName "'$DBM_PACKAGE_NAME'" '
     fi
     if [[  -n ${DBM_VERSION_LEVEL} ]]; then
         CMDLINE=$CMDLINE'-CreatePackage -VersionLevel "'$DBM_VERSION_LEVEL' -AfterCurrentDeployedVersion "'$DBM_AFTER_CURRENT_DEPLOYED_VERSION' "'
