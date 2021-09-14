9f27576a9a9c5ae063f63953406e420f530a0984
Olekasndr Kosenko <sashao@dbmaestro.com>
2021-09-14 12:00:00 +0300

    gl fix

--- src/scripts/get-latest.sh
+++ src/scripts/get-latest.sh
@@ -11,16 +11,16 @@ GetLatest(){
     fi
     CMDLINE='java -jar "'$DBM_TOOL_PATH'/DBmaestroAgent.jar" -GetLatest -ProjectName "'$DBM_PROJECT_NAME'" -EnvName "'$DBM_ENVIRONMENT_NAME'" '
     if [[  -n ${DBM_PACKAGE_NAME} ]]; then
-        CMDLINE=$CMDLINE'-PackageName "'$DBM_PACKAGE_NAME' "'
+        CMDLINE=$CMDLINE'-PackageName "'$DBM_PACKAGE_NAME'" '
     fi
     if [[  -n ${DBM_LABEL_NAME} ]]; then
-        CMDLINE=$CMDLINE'-LabelName "'$DBM_LABEL_NAME' "'
+        CMDLINE=$CMDLINE'-LabelName "'$DBM_LABEL_NAME'" '
     fi
     if [[  -n ${DBM_BASELINE_LABEL_NAME} ]]; then
-        CMDLINE=$CMDLINE'-BaselineLabelName "'$DBM_BASELINE_LABEL_NAME' "'
+        CMDLINE=$CMDLINE'-BaselineLabelName "'$DBM_BASELINE_LABEL_NAME'" '
     fi
     if [[  -n ${DBM_CREATE_DCRIPTS_ONLY} ]]; then
-        CMDLINE=$CMDLINE'-CreateScriptsOnly "'$DBM_CREATE_DCRIPTS_ONLY' "'
+        CMDLINE=$CMDLINE'-CreateScriptsOnly "'$DBM_CREATE_DCRIPTS_ONLY'" '
     fi
     CMDLINE=$CMDLINE' -Server "'$DBM_SERVER_ADDRESS'" -AuthType "'$DBM_AUTH_TYPE'" -UserName "'$DBM_USERNAME'" -Password "'$DBM_PWD'"'
     echo "$CMDLINE"
