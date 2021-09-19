843dc1b0fd8720a1eec5b41096256ebc4e04e015
Olekasndr Kosenko <sashao@dbmaestro.com>
2021-09-19 15:56:28 +0300

    params

--- src/scripts/custom-build.sh
+++ src/scripts/custom-build.sh
@@ -3,8 +3,8 @@
 CustomBuild(){
     if [ -z "${DBM_TOOL_PATH:=${DBMAESTRO_TOOL_PATH:-'tools/dbmaestro'}}" ]; then echo "DBmaestro tool path is not set"&&exit 1; fi
     if [ -z "${DBM_SERVER_ADDRESS:=$DBMAESTRO_SERVER_ADDRESS}" ]; then echo "DBmaestro server address is not set"&&exit 1; fi
-    if [ -z "${DBM_USE_SSL:=${DBMAESTRO_USE_SSL:-'n'}}" ]; then echo "DBmaestro SSL usage is not set"&&exit 1; fi
-    if [ -z "${DBM_AUTH_TYPE:=${DBMAESTRO_AUTH_TYPE:-'DBmaestroAccount'}}" ]; then echo "DBmaestro auth type is not set"&&exit 1; fi
+    if [ -z "${DBM_USE_SSL:=${DBMAESTRO_USE_SSL:-n}}" ]; then echo "DBmaestro SSL usage is not set"&&exit 1; fi
+    if [ -z "${DBM_AUTH_TYPE:=${DBMAESTRO_AUTH_TYPE:-DBmaestroAccount}}" ]; then echo "DBmaestro auth type is not set"&&exit 1; fi
     if [ -z "${DBM_USERNAME:=$DBMAESTRO_USERNAME}" ]; then echo "DBmaestro username is not set"&&exit 1; fi
     if [ -z "$DBMAESTRO_PASSWORD" ]; then echo "DBMAESTRO_PASSWORD variable is not set"&&exit 1; fi
     CMDLINE='java -jar "'$DBM_TOOL_PATH'/DBmaestroAgent.jar" -CustomBuild -SourceProjectName "'$DBM_SOURCE_PROJECT_NAME'" -SourceEnvName "'$DBM_SOURCE_ENVIRONMENT_NAME'" -TargetProjectName "'$DBM_TARGET_PROJECT_NAME'" -TargetEnvName "'$DBM_TARGET_ENVIRONMENT_NAME'" '
